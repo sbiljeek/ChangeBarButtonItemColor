38c3db1e58d440e990b758a6052f237e41530d7e
sbiljeek <sbiljeekdev@gmail.com>
2023-01-26 13:15:00 +0300

    initial commit

--- ChangeBarButtonItemColor/AppDelegate.swift
+++ ChangeBarButtonItemColor/AppDelegate.swift
@@ -13,7 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().tintColor = .systemPink
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemPink]
+        
         return true
     }
 
